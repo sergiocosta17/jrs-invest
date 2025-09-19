2af4c198a92204e91d5670722fedf40f235971fb
sergiocosta17 <sergiocostaaraujoneto@gmail.com>
2025-09-19 17:41:21 -0300

    api local

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import toast from 'react-hot-toast';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: 'http://localhost:3001',
 });
 
 api.interceptors.request.use(async (config) => {
