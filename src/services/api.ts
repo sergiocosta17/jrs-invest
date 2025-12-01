e0b51255fa7acd1062f6d8d6123811920b6a1cfc
sergiocosta17 <sergiocostaaraujoneto@gmail.com>
2025-12-01 18:39:36 -0300

    ajuste para funcionamento do deploy

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import toast from 'react-hot-toast';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3001',
+  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:3001',
 });
 
 api.interceptors.request.use(async (config) => {
